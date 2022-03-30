f5113fd1f09842411b18fc876058888b495743cd
Matt Dale <matt.dale@york.ac.uk>
2022-03-30 12:38:47 +0100

    Add combined metrics

--- Support files/other/Metrics/combinedMetrics.m
+++ Support files/other/Metrics/combinedMetrics.m
@@ -1,8 +1,11 @@
+%% function assesses all given metrics using a single data input 
+
 function metrics = combinedMetrics(individual,config,seed,data_length)
 
 rng(seed,'twister');
 
 metrics = [];
+config.err_type = 'NRMSE';
 
 %% Run network with random input
 % define network sizes
@@ -14,12 +17,17 @@ n_input_units = individual.n_input_units;
 config.wash_out = n_output_units;
 
 %define input signal
-data_sequence = 2*rand(data_length,n_input_units)-1;
+data_sequence = 2*rand(data_length+config.wash_out,n_input_units)-1;
 if config.discrete %strcmp(config.res_type,'elementary_CA') || strcmp(config.res_type,'2d_CA') || strcmp(config.res_type,'RBN')
     data_sequence = floor(heaviside(data_sequence));
 end
 input_sequence = repmat(data_sequence(:,1),1,n_input_units);
 
+% no preprocessing, i.e. rescaling
+config.preprocess = ''; 
+config.preprocess_shift = [];
+[input_sequence, config] = EncodeData(input_sequence,config);
+            
 %kernel matrix - pick 'to' at halfway point
 states = config.assessFcn(individual,input_sequence,config);
 
@@ -29,73 +37,75 @@ states(isinf(states)) = 0;
 
 % split states
 sequence_length = data_length/2;
+data_length = length(data_sequence);
 train_states = states(1:sequence_length,:);
 test_states = states(1+sequence_length:end,:);
 
 % Find best reg parameter
 reg_param = [10e-7];
-config.err_type = 'NMSE';
+%config.err_type = 'NMSE';
 
 %% define input data
 for n_metrics = 1:length(config.metrics_names)
     
     switch(config.metrics_names{n_metrics})
         
-        case 'L-MC'
-            try load('L_MCdata.mat'); 
+        case 'L_MC'
+            try load(strcat('L_MCdata',num2str(individual.total_units),'.mat')); 
             catch mem_input_sequence = zeros(size(input_sequence(config.wash_out+1:data_length)));
             end
             if ~isequal(mem_input_sequence,input_sequence(config.wash_out+1:data_length))
                
                 mem_input_sequence = input_sequence(config.wash_out+1:data_length);
                 
-                for i = 1:n_output_units
+                for i = 1:n_internal_units%n_output_units
                     mem_output_sequence(:,i) = input_sequence(config.wash_out+1-i:data_length-i);
                 end
-                save('L_MCdata.mat','mem_input_sequence','mem_output_sequence');
+                save(strcat('L_MCdata',num2str(individual.total_units),'.mat'),'mem_input_sequence','mem_output_sequence');
             end
             
-        case 'Q-MC'
-            try load('Q_MCdata.mat'); 
+        case 'Q_MC' %Quadratic memory: yk(n)=(3u^2(n-k)-1)/2  ----- taken from Duport et al. and Ortin and Pesquera (2019, doi.org/10.3389/fphy.2019.00210)
+            try load(strcat('Q_MCdata',num2str(individual.total_units),'.mat')); 
             catch quad_input_sequence = zeros(size(input_sequence(config.wash_out+1:data_length)));
             end
             if ~isequal(quad_input_sequence,input_sequence(config.wash_out+1:data_length))
                 quad_input_sequence = input_sequence(config.wash_out+1:data_length);
-                for k = 1:n_output_units
-                    for n = 1:length(quad_input_sequence)
-                        if n > k
-                            quad_output_sequence(n,k) = (3.*(quad_input_sequence(n-k,1).^2)) - 1;
-                        else
-                            quad_output_sequence(n,k) = quad_input_sequence(n,1);
-                        end
-                    end
+                for i = 1:n_internal_units%n_output_units%n_internal_units
+                    quad_output_sequence(:,i) = ((input_sequence(config.wash_out+1-i:data_length-i).^2)*3 -1)/2;
                 end
-                save('Q_MCdata.mat','quad_input_sequence','quad_output_sequence');
+                
+                save(strcat('Q_MCdata',num2str(individual.total_units),'.mat'),'quad_input_sequence','quad_output_sequence');
             end
-        case 'C-MC'
-            try load('C_MCdata.mat'); 
+            
+        case 'C_MC' %Cubic memory: yk(n)=(5u^3(n-k)-3u(n-k))/2
+            try load(strcat('C_MCdata',num2str(individual.total_units),'.mat'));
+            catch cubic_input_sequence = zeros(size(input_sequence(config.wash_out+1:data_length)));
+            end
+            if ~isequal(cubic_input_sequence,input_sequence(config.wash_out+1:data_length))
+                cubic_input_sequence = input_sequence(config.wash_out+1:data_length);
+                for i = 1:n_internal_units%n_output_units
+                    cubic_output_sequence(:,i) =  (5 * (input_sequence(config.wash_out+1-i:data_length-i).^3) - (3 * input_sequence(config.wash_out+1-i:data_length-i)))/2;
+                end
+                
+                save(strcat('C_MCdata',num2str(individual.total_units),'.mat'),'cubic_input_sequence','cubic_output_sequence');
+            end
+            
+        case 'X_MC' %Cross memory: yk,k'=u(n-k)·u(n-k') summing over k,k' for k < k'
+            try load(strcat('X_MCdata',num2str(individual.total_units),'.mat')); 
             catch cross_input_sequence = zeros(size(input_sequence(config.wash_out+1:data_length)));
             end
             if ~isequal(cross_input_sequence,input_sequence(config.wash_out+1:data_length))
                 cross_input_sequence = input_sequence(config.wash_out+1:data_length);
-                for n = 1:length(cross_input_sequence)
-                    cnt = 1;
-                    for k = 1:n_output_units
-                        for k_p = 1:n_output_units
-                            if (n > k && n > k_p) && k ~= k_p
-                                %crossFcn(k,k_p) = cross_input_sequence(n-k,1)*cross_input_sequence(n-k_p,1);
-                                cross_output_sequence(n,cnt) = cross_input_sequence(n-k,1)*cross_input_sequence(n-k_p,1);
-                            else
-                                %crossFcn(k,k_p) = 0;
-                                cross_output_sequence(n,cnt) = 0;
-                            end
+                cnt = 1;
+                for k = 1:n_internal_units
+                    for k_p = 1:n_internal_units%n_output_units
+                        if k < k_p
+                            cross_output_sequence(:,cnt) = input_sequence(config.wash_out+1-k:data_length-k,1).*input_sequence(config.wash_out+1-k_p:data_length-k_p,1);
                             cnt = cnt+1;
                         end
-                        %cross_output_sequence(n,k) = sum(crossFcn(k,:));
                     end
-                    % cross_output_sequence(n,:) = crossFcn(:);
                 end
-                save('C_MCdata.mat','cross_input_sequence','cross_output_sequence');
+                save(strcat('X_MCdata',num2str(individual.total_units),'.mat'),'cross_input_sequence','cross_output_sequence');
             end
         otherwise
     end
@@ -125,113 +135,58 @@ for n_metrics = 1:length(config.metrics_names)
             
             metrics = [metrics kernel_rank];
             
-        case 'L-MC'
-            %% calculate linear memory capacity (MC)            
-            train_input_sequence = mem_input_sequence(1:sequence_length,:);
-            test_input_sequence = mem_input_sequence(1+sequence_length:end,:);
+        case 'L_MC'
+            %% calculate linear memory capacity (MC)                       
+            C_y = calculateCapacity(mem_output_sequence(1:sequence_length,:),mem_output_sequence(1+sequence_length:end,:)...
+                ,train_states,test_states,reg_param,config);
             
-            train_output_sequence = mem_output_sequence(1:sequence_length,:);
-            test_output_sequence = mem_output_sequence(1+sequence_length:end,:);
+            metrics = [metrics C_y];            
             
-            % get output and weights
-            Y = calculateWeights(train_states,train_output_sequence,test_states,test_output_sequence,reg_param,config);
+        case 'Q_MC'
+            %% %% quadratic memory capacity - (y_k(n)= 3u^2(n-k)-1)/2       
+            C_y = calculateCapacity(quad_output_sequence(1:sequence_length,:),quad_output_sequence(1+sequence_length:end,:)...
+                ,train_states,test_states,reg_param,config);
             
-            MC_k= 0; Cm = 0;
-            test_in_var = test_input_sequence(:,1);
-            targVar = 1/(length(test_in_var)-1) * sum((test_in_var-mean(test_in_var)).*(test_in_var-mean(test_in_var)));
+            metrics = [metrics C_y];
             
-            for i = 1:n_output_units
-                
-                coVar = 1/(length(Y(:,i))-1) * sum((test_output_sequence(:,i)-mean(test_output_sequence(:,i)))...
-                    .*(Y(:,i)-mean(Y(:,i))));
-                outVar = 1/(length(Y(:,i))-1) * sum((Y(:,i)-mean(Y(:,i))).*(Y(:,i)-mean(Y(:,i))));
-                totVar = (outVar*targVar(1));
-                MC_k(i) = (coVar*coVar)/totVar;
-                
-                %remove low values from measure
-                if MC_k(i) <= 0.1
-                    MC_k(i) = 0;
-                end
-            end
+        case 'C_MC'
+            %% %% cubic memory capacity - yk(n)=(5u^3(n-k)-3u(n-k))/2     
+            C_y = calculateCapacity(cubic_output_sequence(1:sequence_length,:),cubic_output_sequence(1+sequence_length:end,:)...
+                ,train_states,test_states,reg_param,config);
             
-            MC_k(isnan(MC_k)) = 0;
-            MC = sum(MC_k);
+            metrics = [metrics C_y];
             
-            metrics = [metrics MC];
-            
-        case 'Q-MC'
-            %% %% quadratic memory capacity - y_k(n)=3u^2(n-k)-1       
-            train_input_sequence = quad_input_sequence(1:sequence_length,:);
-            test_input_sequence = quad_input_sequence(1+sequence_length:end,:);
-            
-            train_output_sequence = quad_output_sequence(1:sequence_length,:);
-            test_output_sequence = quad_output_sequence(1+sequence_length:end,:);
-            
-            % get output and weights
-            Y = calculateWeights(train_states,train_output_sequence,test_states,test_output_sequence,reg_param,config);
-            
-            % calculate output
-            for i = 1:size(Y,2)
-                
-                nmse = (mean((test_output_sequence(:,i)-Y(:,i)).^2)/mean(test_output_sequence(:,i).^2));
-                
-                if nmse > 1
-                    nmse = 1;
-                end
-                
-                quad_MC(i) = 1 - nmse;
-                
-                %remove low values from measure
-                if quad_MC(i) <= 0.1
-                    quad_MC(i) = 0;
-                end
-                
-            end
-            quad_MC = sum(quad_MC);
-            
-            metrics = [metrics quad_MC];
-            
-        case 'C-MC'
+        case 'X_MC'
             %% cross memory capacity - y_kk'(n)=u(n-k)u(n-k')
-            train_input_sequence = cross_input_sequence(1:sequence_length,:);
-            test_input_sequence = cross_input_sequence(1+sequence_length:end,:);
+            C_y = calculateCapacity(cross_output_sequence(1:sequence_length,:),cross_output_sequence(1+sequence_length:end,:)...
+                ,train_states,test_states,reg_param,config);
             
-            train_output_sequence = cross_output_sequence(1:sequence_length,:);
-            test_output_sequence = cross_output_sequence(1+sequence_length:end,:);
-            
-            % get output and weights
-            Y = calculateWeights(train_states,train_output_sequence,test_states,test_output_sequence,reg_param,config);
-            
-            % calculate output
-            for i = 1:size(Y,2)
-                
-                if sum(test_output_sequence(:,i)) > 0
-                    nmse = (mean((test_output_sequence(:,i)-Y(:,i)).^2)/mean(test_output_sequence(:,i).^2));
-                    
-                    if nmse > 1
-                        nmse = 1;
-                    end
-                    
-                    cross_MC(i) = 1 - nmse;
-                    
-                    %remove low values from measure
-                    if cross_MC(i) <= 0.1
-                        cross_MC(i) = 0;
-                    end
-                    
-                else
-                    cross_MC(i) = 0;
-                end
-                
-            end
-            cross_MC = sum(cross_MC);
-            
-            metrics = [metrics cross_MC];
+            metrics = [metrics C_y];
             
         otherwise
             
     end
 end
+end
+
+function C_y = calculateCapacity(train_output_sequence,test_output_sequence,train_states,test_states,reg_param,config)
+
+Y = calculateWeights(train_states,train_output_sequence,test_states,test_output_sequence,reg_param,config);
+
+% calculate output
+for i = 1:size(Y,2)
+    
+    C_yk(i) = 1 - (sum((Y(:,i)-test_output_sequence(:,i)).^2)/sum(test_output_sequence(:,i).^2));
+    
+    %remove low values from measure
+    if C_yk(i) < 0.1
+        C_yk(i) = 0;
+    end
+    
+end
+
+C_y = sum(C_yk);
+
 end
 
 function [Y,output_train_sequence,output_test_sequence,output_weights] = calculateWeights(train_states,train_output_sequence,test_states,test_output_sequence,reg_param,config)
